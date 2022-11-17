4f7ea970f4c63552b2aff7c3710c641fa9dd5be0
anast <anast@DESKTOP-JR1O9R5>
2022-11-17 10:14:01 +0300

    Добавьте файлы проекта.

---
+++ Calculator1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Calculator1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("calculator");
+            int a = 
+        }
+    }
+}
